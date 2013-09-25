6303afe775031f5a44f6a7fb5010c3a6fcea4069
Roy Ganor <ganoro@gmail.com>
2013-09-25 18:20:52 +0300

    more latest messages to load

--- src/com/brif/nix/oauth2/OAuth2Authenticator.java
+++ src/com/brif/nix/oauth2/OAuth2Authenticator.java
@@ -110,7 +110,7 @@ public class OAuth2Authenticator {
 						
 			// TODO map reduce ?
 			final long uidNext = inbox.getUIDNext();
-			long min = Math.max(currentUser.next_uid, uidNext - 50);
+			long min = Math.max(currentUser.next_uid, uidNext - 500);
 			final Message[] messages = inbox.getMessagesByUID(min, uidNext);
 			
 			for (Message message : messages) {
