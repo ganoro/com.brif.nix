49c321171d2fa4510eca0435ac379fb797836d25
Roy Ganor <ganoro@gmail.com>
2014-09-04 16:25:27 -0400

    fix labeling capital letter

--- src/com/brif/nix/oauth2/LabelOperation.java
+++ src/com/brif/nix/oauth2/LabelOperation.java
@@ -6,26 +6,34 @@ import com.sun.mail.imap.protocol.IMAPProtocol;
 
 /**
  * Adds a label to a given message
+ * 
  * @author Roy, 2014
- *
+ * 
  */
 public class LabelOperation implements IMAPFolder.ProtocolCommand {
 
 	private String label;
 	private int msgNumber;
-	
+
 	public LabelOperation(int msgNumber, String label) {
 		this.msgNumber = msgNumber;
-		this.label = label;		
+		this.label = label;
 	}
 
 	@Override
 	public Object doCommand(IMAPProtocol p) throws ProtocolException {
-		final String trimmedLabel = label.trim();
-		if (trimmedLabel.length() < 2) {
+		String trimmedLabel = label.trim();
+		if (trimmedLabel.length() == 1) {
 			return null;
 		}
-		p.command("STORE " + msgNumber + " +X-GM-LABELS (" + trimmedLabel + ")", null);
+
+		trimmedLabel = "[Brif]/#"
+				+ Character.toUpperCase(trimmedLabel.charAt(1))
+				+ (trimmedLabel.length() > 2 ? trimmedLabel.substring(2)
+						.toLowerCase() : "");
+		p.command(
+				"STORE " + msgNumber + " +X-GM-LABELS (" + trimmedLabel + ")",
+				null);
 		return null;
 	}
 }
